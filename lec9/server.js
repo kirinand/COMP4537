3f1050de6a88469ad909f8c426c04e1faa7d8543
Kirin Tang <ktang91@my.bcit.ca>
2023-11-15 09:24:13 -0800

    port

--- lec9/server.js
+++ lec9/server.js
@@ -54,6 +54,6 @@ const server = http.createServer((req, res) => {
     res.end('404 Not Found');
   }
 });
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });//**  Generated mostly by chatGPT ver. 4 **/
